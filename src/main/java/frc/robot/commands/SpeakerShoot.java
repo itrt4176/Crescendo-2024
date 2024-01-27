06c887f9e4947e9ccda19ef3124e849c1f80ce99
Puqi Luo <ptluo06@gmail.com>
2024-01-27 11:59:43 -0500

    Added a set method to shooter and added a simple command

---
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.ShooterConstants.*;
+
+public class SpeakerShoot extends Command {
+
+  private final ShooterSubsystem shooter;
+  //add climber later
+  //add intake later
+
+  /** Creates a new SpeakerShoot. */
+  public SpeakerShoot(ShooterSubsystem shooter) {
+    this.shooter = shooter;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooter.set(ShooterConstants.SpeakerShotSpeed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkMax;
+import frc.robot.Constants.ShooterConstants;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class ShooterSubsystem extends SubsystemBase {
+
+  public int gear = 0;
+  public boolean running = false;
+
+  CANSparkMax main = new CANSparkMax(ShooterConstants.kMainShooterMotor, MotorType.kBrushless);
+  CANSparkMax sub = new CANSparkMax(ShooterConstants.kSubShooterMotor, MotorType.kBrushless);
+
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+
+    main.setInverted(false);
+    sub.follow(main, true);
+
+    main.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    sub.setIdleMode(CANSparkMax.IdleMode.kBrake);
+  }
+
+  /**
+   * Increases motor speed and starts the motor if it is not running.
+   */
+  public void shiftUp(){
+    // if(gear < 3 && running){
+    //   gear++;
+    //   start();
+    // }
+    if(gear < 3){
+      gear++;
+      switchGear(gear);
+    }
+  }
+
+  /**
+   * Decreases motor speed and starts the motor
+   */
+  public void shiftDown(){
+    // if(gear > 0 && running){
+    //   gear--;
+    //   start();
+    // }
+    if(gear > 0){
+      gear--;
+      switchGear(gear);
+    }
+  }
+
+  public void start(){
+    if (running == false) {
+      gear = 0;
+      running = true;
+      switchGear(gear);
+    }
+  }
+
+  public void stop() {
+    running = false;
+    main.set(0);
+  }
+
+  private void switchGear(int gear) {
+    switch(gear){
+      case 0:
+      main.set(.25);
+      break;
+      case 1:
+      main.set(.5);
+      break;
+      case 2:
+      main.set(.75);
+      break;
+      case 3:
+      main.set(1);
+      break;
+    }
+  }
+
+  public void set(double speed) {
+    running = true;
+    main.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
