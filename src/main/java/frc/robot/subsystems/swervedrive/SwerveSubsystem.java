895530be56f1ec68b7af98032ed0ffdafa4dcb41
Evan Visnauskis <efvisnauskis@gmail.com>
2024-03-10 01:50:07 -0500

    Turned replanning config to true, true; heading is true

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -125,7 +125,7 @@ public class SwerveSubsystem extends SubsystemBase {
           // Max module speed, in m/s
           swerveDrive.swerveDriveConfiguration.getDriveBaseRadiusMeters(),
           // Drive base radius in meters. Distance from robot center to furthest module.
-          new ReplanningConfig(false, false)
+          new ReplanningConfig(true, true)
           // Default path replanning config. See the API for the options here
         ),
         () -> {
