188eaa4e99b07e3892f2442f1cc69ca8da5aa5c8
Puqi Luo <ptluo06@gmail.com>
2024-03-20 20:01:49 -0400

    Extracted vision class from PhotonLib example repository, added necessary (unchanged) constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 
@@ -70,9 +78,25 @@ public final class Constants {
   }
 
 
-  public static class ClimberConstants{
-    public static final int FORWARD_LIMIT_DIO = 0;
-    public static final int REVERSE_LIMIT_DIO = 1;
-    public static final double FLIPPER_ROTATIONS_TO_DEGREES = -1.0 * 360.0 / (50.0 * (22.0 / 16.0)); //VERIFY!!!
+public static class ClimberConstants{
+  public static final int FORWARD_LIMIT_DIO = 0;
+  public static final int REVERSE_LIMIT_DIO = 1;
+  public static final double FLIPPER_ROTATIONS_TO_DEGREES = -1.0 * 360.0 / (50.0 * (22.0 / 16.0)); //VERIFY!!!
+}
+
+public static class Vision {
+  public static final String kCameraName = "YOUR CAMERA NAME";
+  // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+  public static final Transform3d kRobotToCam =
+          new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+  // The layout of the AprilTags on the field
+  public static final AprilTagFieldLayout kTagLayout =
+          AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+
+  // The standard deviations of our vision estimated poses, which affect correction rate
+  // (Fake values. Experiment and determine estimation noise on an actual robot.)
+  public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+  public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
   }
 }
